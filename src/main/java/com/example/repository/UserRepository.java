f2ed5f78467cb1649f23ee1c9ac1e5c0870ab800
apps-f9 <f9app.devops@gmail.com>
2024-03-13 19:27:57 -0400

    initial commit

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.example.repository;
 import com.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
