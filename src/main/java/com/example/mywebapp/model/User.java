72b1de64542c39eda65ac1c39eea512bb8a23ab4
apps-f9 <f9app.devops@gmail.com>
2024-03-26 23:55:14 -0400

    run the app

--- src/main/java/com/example/mywebapp/model/User.java
+++ src/main/java/com/example/mywebapp/model/User.java
@@ -1,3 +1,5 @@
+package com.example.mywebapp.model;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,38 +13,5 @@ public class User {
     private String name;
     private String email;
 
-    public User() {
-        // Default constructor
-    }
-
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    // Getters and setters for id, name, and email
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // Constructors, getters, setters
 }
