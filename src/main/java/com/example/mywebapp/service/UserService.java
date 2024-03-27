479089dabfba39ed97729565ae652e33ffa5c734
apps-f9 <f9app.devops@gmail.com>
2024-03-26 23:42:49 -0400

    run the app

--- src/main/java/com/example/mywebapp/service/UserService.java
+++ src/main/java/com/example/mywebapp/service/UserService.java
@@ -5,15 +5,20 @@ import com.example.mywebapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class UserService {
+
+    private static final Logger LOGGER = Logger.getLogger(UserService.class.getName());
 
     @Autowired
     private UserRepository userRepository;
 
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        List<User> users = userRepository.findAll();
+        LOGGER.info("Retrieved " + users.size() + " users from the database");
+        return users;
     }
 
     // Other methods as needed
